2f984be0348d773acc38fbea9ffbf805e1cda258
Yusuke Doi <yuusukedoi23@gmail.com>
2020-12-29 11:05:04 +0900

    名前修正

--- lib/solver.rb
+++ lib/solver.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def solve(input)
   parts = input.scan(/.{2}/)
-  answer = number_ok(parts)
+  answer = bingo(parts)
   if answer.empty?
     'none'
   else
@@ -10,28 +10,28 @@ def solve(input)
   end
 end
 
-def number_ok(parts)
+def bingo(parts)
   parts.combination(3).select do |combi|
-    num_combi_ok(combi.map{|e| e[1].to_i}.sort) && alphabet_combi_ok(combi.map{|e| e[0]}.sort)
+    num_combi_bingo?(combi.map{|e| e[1]}.sort) && alphabet_combi_bingo?(combi.map{|e| e[0]}.sort)
   end
 end
 
-def num_combi_ok(three_nums)
-  bingo = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
+def num_combi_bingo?(three_nums)
+  bingo_patterns = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+    ['1', '5', '9'],
+    ['3', '5', '7']
   ]
-  bingo.include?(three_nums)
+  bingo_patterns.include?(three_nums)
 end
 
-def alphabet_combi_ok(three_alphabets)
-  bingo = [
+def alphabet_combi_bingo?(three_alphabets)
+  bingo_patterns = [
     ['A', 'B', 'C'],
     ['D', 'E', 'F'],
     ['G', 'H', 'J'],
@@ -41,5 +41,5 @@ def alphabet_combi_ok(three_alphabets)
     ['A', 'E', 'J'],
     ['C', 'E', 'G']
   ]
-  bingo.include?(three_alphabets)
+  bingo_patterns.include?(three_alphabets)
 end
